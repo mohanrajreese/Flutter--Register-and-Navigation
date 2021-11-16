58d2a26d7192cdb114b1490f302ef12f22d1c15a
mohanrajreese <mohanrj1345@gmail.com>
2021-11-16 14:08:43 +0530

    Flutter Excercise-MAD

---
+++ lib/main.dart
@@ -0,0 +1,349 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Exercise 2',
+      theme: ThemeData(
+        primarySwatch: Colors.pink
+      ),
+      home: MyHomePage(title: 'Enter Your Data'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  String? firstName;
+  String? lastName;
+  String? phoneNumber;
+  String? address;
+  String? email;
+  String? gender;
+  DateTime selectedDate = DateTime.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(1990),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: 30),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          
+                          onSaved: (newValue) => firstName = newValue,
+                          onChanged: (value) {
+                            firstName = value;
+                          },
+                          validator: (value) {},
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "First Name",
+                            hintText: "Enter your first name",
+                            floatingLabelBehavior: FloatingLabelBehavior.auto,
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        TextFormField(
+                          onSaved: (newValue) => lastName = newValue,
+                          onChanged: (value) {
+                            lastName = value;
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Last Name",
+                            hintText: "Enter your last name",
+                            floatingLabelBehavior: FloatingLabelBehavior.auto,
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        TextFormField(
+                          keyboardType: TextInputType.phone,
+                          onSaved: (newValue) => phoneNumber = newValue,
+                          onChanged: (value) {
+                            phoneNumber = value;
+                          },
+                          validator: (value) {},
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Phone Number",
+                            hintText: "Enter your phone number",
+                            // hintTextDirection: Center,
+                            floatingLabelBehavior: FloatingLabelBehavior.auto,
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          onSaved: (newValue) => email = newValue,
+                          onChanged: (value) {
+                            email = value;
+                          },
+                          validator: (value) {},
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Email",
+                            hintText: "Enter your email",
+                            floatingLabelBehavior: FloatingLabelBehavior.auto,
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        TextFormField(
+                          keyboardType: TextInputType.datetime,
+                          // onSaved: (newValue) => birthday = newValue,
+                          // onChanged: (value) {},
+                          // validator: (value) {},
+                          onTap: () => _selectDate(context),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Birthday",
+                            // icon: Icons.calendar_today_outlined,
+                            hintText: "${selectedDate.toLocal()}".split(' ')[0],
+                            floatingLabelBehavior: FloatingLabelBehavior.auto,
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        DropdownButtonFormField(
+                          // border: OutlineInputBorder(),
+                          items: ['Male', 'Female','Third Gender','Prefer not to say']
+                              .map((String category) {
+                            return DropdownMenuItem(
+                                value: category,
+                                child: Row(
+                                  children: <Widget>[
+                                    Text(category),
+                                  ],
+                                ));
+                          }).toList(),
+                          onChanged: (newValue) {
+                            setState(() => gender = newValue as String?);
+                          },
+                          value: gender,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Colors.white,
+                            labelText: "Gender",
+                            hintText: "Gender",
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        TextFormField(
+                          keyboardType: TextInputType.streetAddress,
+                          onSaved: (newValue) => address = newValue,
+                          onChanged: (value) {
+                            address = value;
+                          },
+                          validator: (value) {},
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Address",
+                            hintText: "Enter your  address",
+                            // If  you are using latest version of flutter then lable text and hint text shown like this
+                            // if you r using flutter less then 1.20.* then maybe this is not working properly
+                            floatingLabelBehavior: FloatingLabelBehavior.auto,
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        DefaultButton(
+                          text: "Save",
+                          press: () {
+                            if (_formKey.currentState!.validate()) {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => DisplayDetails(
+                                    email: email,
+                                    firstname: firstName,
+                                    lastname: lastName,
+                                    gender: gender,
+                                    phoneNumber: phoneNumber,
+                                    address: address,
+                                    birthday: "${selectedDate.toLocal()}"
+                                        .split(' ')[0],
+                                  ),
+                                ),
+                              );
+                            }
+                          },
+                        ),
+                        SizedBox(height: 30),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DisplayDetails extends StatefulWidget {
+  final String? firstname;
+  final String? lastname;
+  final String? email;
+  final String? gender;
+  final String? phoneNumber;
+  final String? address;
+  final String? birthday;
+  DisplayDetails(
+    {
+    this.firstname,
+    this.lastname,
+    this.email,
+    this.gender,
+    this.phoneNumber,
+    this.address,
+    this.birthday,
+  }
+  );
+
+  @override
+  _DisplayDetailsState createState() => _DisplayDetailsState();
+}
+
+class _DisplayDetailsState extends State<DisplayDetails> 
+{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "DISPLAY DETAILS",
+            style: TextStyle(color: Colors.white, fontSize: 26, fontWeight: FontWeight.bold,),
+          ),
+        ),
+        body: Container(
+          margin: EdgeInsets.fromLTRB(5, 50, 0, 15),
+          width: double.infinity,
+          // height: ,
+          padding: EdgeInsets.all(10.0),
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            color: Colors.grey[200],
+            elevation: 100,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                  title: Text('First Name - ${widget.firstname}',
+                      style: TextStyle(fontSize: 18.0)),
+                  subtitle: Text("", style: TextStyle(fontSize: 18.0)),
+                ),
+                ListTile(
+                  title: Text('Last Name - ${widget.lastname}',
+                      style: TextStyle(
+                        fontSize: 18.0)
+                        ),
+                  subtitle: Text('', style: TextStyle(fontSize: 18.0)),
+                ),
+                ListTile(
+                  title: Text('Email - ${widget.email}',
+                      style: TextStyle(fontSize: 18.0)),
+                  subtitle: Text("", style: TextStyle(fontSize: 18.0)),
+                ),
+                ListTile(
+                  title: Text('Phone Number - ${widget.phoneNumber}',
+                      style: TextStyle(fontSize: 18.0)),
+                  subtitle: Text('', style: TextStyle(fontSize: 18.0)),
+                ),
+                ListTile(
+                  title: Text('Birthday - ${widget.birthday}',
+                      style: TextStyle(fontSize: 18.0)),
+                  subtitle: Text('', style: TextStyle(fontSize: 18.0)),
+                ),
+                ListTile(
+                  title: Text('Gender - ${widget.gender}',
+                      style: TextStyle(fontSize: 18.0)),
+                  subtitle: Text("", style: TextStyle(fontSize: 18.0)),
+                ),
+                ListTile(
+                  title: Text('Address - ${widget.address}',
+                      style: TextStyle(fontSize: 18.0)),
+                  subtitle: Text("", style: TextStyle(fontSize: 18.0)),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class DefaultButton extends StatelessWidget {
+  const DefaultButton({
+    Key? key,
+    this.text,
+    this.press,
+  }) : super(key: key);
+  final String? text;
+  final Function? press;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 56,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          primary: Colors.white,
+          backgroundColor: Colors.blue,
+        ),
+        onPressed: press as void Function()?,
+        child: Text(
+          text!,
+          style: TextStyle(
+            fontSize: 180,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
